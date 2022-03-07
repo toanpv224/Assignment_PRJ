d49fe3976d8062a623243aeb1ad2062561abf92b
toanpv224 <toanpv224@gmail.com>
2022-03-07 07:59:48 +0700

    Update Home_jsp.java

--- Ass/build/generated/src/org/apache/jsp/Home_jsp.java
+++ Ass/build/generated/src/org/apache/jsp/Home_jsp.java
@@ -13,7 +13,7 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
 
   private static java.util.List<String> _jspx_dependants;
 
-  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_forEach_end_begin;
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_forEach_var_items;
 
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
@@ -22,11 +22,11 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
   }
 
   public void _jspInit() {
-    _jspx_tagPool_c_forEach_end_begin = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _jspx_tagPool_c_forEach_var_items = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
   }
 
   public void _jspDestroy() {
-    _jspx_tagPool_c_forEach_end_begin.release();
+    _jspx_tagPool_c_forEach_var_items.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -109,8 +109,8 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
         return;
       out.write("\r\n");
       out.write("\r\n");
-      out.write("                </div>\r\n");
-      out.write("            </section>\r\n");
+      out.write("            </div>\r\n");
+      out.write("        </section>\r\n");
       out.write("\r\n");
       out.write("        ");
       org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "Footer.jsp", out, false);
@@ -143,11 +143,11 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
     PageContext pageContext = _jspx_page_context;
     JspWriter out = _jspx_page_context.getOut();
     //  c:forEach
-    org.apache.taglibs.standard.tag.rt.core.ForEachTag _jspx_th_c_forEach_0 = (org.apache.taglibs.standard.tag.rt.core.ForEachTag) _jspx_tagPool_c_forEach_end_begin.get(org.apache.taglibs.standard.tag.rt.core.ForEachTag.class);
+    org.apache.taglibs.standard.tag.rt.core.ForEachTag _jspx_th_c_forEach_0 = (org.apache.taglibs.standard.tag.rt.core.ForEachTag) _jspx_tagPool_c_forEach_var_items.get(org.apache.taglibs.standard.tag.rt.core.ForEachTag.class);
     _jspx_th_c_forEach_0.setPageContext(_jspx_page_context);
     _jspx_th_c_forEach_0.setParent(null);
-    _jspx_th_c_forEach_0.setBegin(1);
-    _jspx_th_c_forEach_0.setEnd(12);
+    _jspx_th_c_forEach_0.setItems((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${sessionScope.listP}", java.lang.Object.class, (PageContext)_jspx_page_context, null));
+    _jspx_th_c_forEach_0.setVar("o");
     int[] _jspx_push_body_count_c_forEach_0 = new int[] { 0 };
     try {
       int _jspx_eval_c_forEach_0 = _jspx_th_c_forEach_0.doStartTag();
@@ -156,109 +156,38 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
           out.write("\r\n");
           out.write("\r\n");
           out.write("                    <div class=\"col-md-3 col-sm-6 col-xs-12 product\">\r\n");
-          out.write("                        <div\r\n");
-          out.write("                            class=\"product product-grid clearfix single-product\"\r\n");
-          out.write("                            >\r\n");
-          out.write("                            <div data-id=\"product-1038149669\">\r\n");
-          out.write("                                <div class=\"product-media\">\r\n");
-          out.write("                                    <div class=\"product-img product-thumbnail img-full\">\r\n");
-          out.write("                                        <a\r\n");
-          out.write("                                            href=\"/products/ao-thun-degrey-nhieu-trang-atdn-trang\"\r\n");
-          out.write("                                            title=\"\"\r\n");
-          out.write("                                            >\r\n");
-          out.write("                                            <picture class=\"loop-one-img\">\r\n");
-          out.write("                                                <source\r\n");
-          out.write("                                                    media=\"(max-width: 767px)\"\r\n");
-          out.write("                                                    data-srcset=\"//product.hstatic.net/1000281824/product/1525_e62ba458922847ce98f35002ac682dce_large.jpg\"\r\n");
-          out.write("                                                    />\r\n");
-          out.write("                                                <source\r\n");
-          out.write("                                                    media=\"(min-width: 768px)\"\r\n");
-          out.write("                                                    data-srcset=\"//product.hstatic.net/1000281824/product/1525_e62ba458922847ce98f35002ac682dce_grande.jpg\"\r\n");
-          out.write("                                                    />\r\n");
-          out.write("                                                <img\r\n");
-          out.write("                                                    class=\"img-loop lazyloaed\"\r\n");
-          out.write("                                                    height=\"300\"\r\n");
-          out.write("                                                    width=\"300\"\r\n");
-          out.write("                                                    alt=\" Áo Thun Degrey Nhiễu Trắng - ATDN Trắng \"\r\n");
-          out.write("                                                    data-srcset=\"//product.hstatic.net/1000281824/product/1525_e62ba458922847ce98f35002ac682dce_grande.jpg\"\r\n");
-          out.write("                                                    src=\"//product.hstatic.net/1000281824/product/1525_e62ba458922847ce98f35002ac682dce_grande.jpg\"\r\n");
-          out.write("                                                    />\r\n");
-          out.write("                                            </picture>\r\n");
-          out.write("\r\n");
-          out.write("                                            <picture class=\"loop-two-img\">\r\n");
-          out.write("                                                <source\r\n");
-          out.write("                                                    media=\"(max-width: 767px)\"\r\n");
-          out.write("                                                    data-srcset=\"//product.hstatic.net/1000281824/product/1526_bcea15cadb424469b6a2c01eecbf07a0_large.jpg\"\r\n");
-          out.write("                                                    />\r\n");
-          out.write("                                                <source\r\n");
-          out.write("                                                    media=\"(min-width: 768px)\"\r\n");
-          out.write("                                                    data-srcset=\"//product.hstatic.net/1000281824/product/1526_bcea15cadb424469b6a2c01eecbf07a0_grande.jpg\"\r\n");
-          out.write("                                                    />\r\n");
-          out.write("                                                <img\r\n");
-          out.write("                                                    class=\"img-loop img-hover lazyloaed\"\r\n");
-          out.write("                                                    height=\"300\"\r\n");
-          out.write("                                                    width=\"300\"\r\n");
-          out.write("                                                    alt=\" Áo Thun Degrey Nhiễu Trắng - ATDN Trắng \"\r\n");
-          out.write("                                                    data-srcset=\"//product.hstatic.net/1000281824/product/1526_bcea15cadb424469b6a2c01eecbf07a0_grande.jpg\"\r\n");
-          out.write("                                                    src=\"//product.hstatic.net/1000281824/product/1526_bcea15cadb424469b6a2c01eecbf07a0_grande.jpg\"\r\n");
-          out.write("                                                    />\r\n");
-          out.write("                                            </picture>\r\n");
-          out.write("                                        </a>\r\n");
-          out.write("                                    </div>\r\n");
-          out.write("\r\n");
-          out.write("                                    <div class=\"product-hover\">\r\n");
-          out.write("                                        <div class=\"product-actions\">\r\n");
-          out.write("                                            <a\r\n");
-          out.write("                                                href=\"javascript:void(0)\"\r\n");
-          out.write("                                                data-id=\"ao-thun-degrey-nhieu-trang-atdn-trang\"\r\n");
-          out.write("                                                class=\"awe-button product-quick-view btn-quickview\"\r\n");
-          out.write("                                                data-toggle=\"tooltip\"\r\n");
-          out.write("                                                data-placement=\"left\"\r\n");
-          out.write("                                                title=\"Xem nhanh\"\r\n");
-          out.write("                                                >\r\n");
-          out.write("                                                <i class='bx bxs-detail quickview' ></i>Xem\r\n");
-          out.write("                                            </a>\r\n");
-          out.write("                                            <form\r\n");
-          out.write("                                                action=\"/cart/add\"\r\n");
-          out.write("                                                method=\"post\"\r\n");
-          out.write("                                                class=\"variants AddToCartForm-1038149669\"\r\n");
-          out.write("                                                enctype=\"multipart/form-data\"\r\n");
-          out.write("                                                >\r\n");
-          out.write("                                                <a\r\n");
-          out.write("                                                    class=\"btn-select-option product-add-cart\"\r\n");
-          out.write("                                                    data-toggle=\"tooltip\"\r\n");
-          out.write("                                                    data-placement=\"left\"\r\n");
-          out.write("                                                    title=\"Thêm vào giỏ hàng\"\r\n");
-          out.write("                                                    href=\"#####################################################################################\"\r\n");
-          out.write("                                                    ><i class=\"bx bxs-cart-add addcart\"></i>Thêm\r\n");
-          out.write("                                                </a>\r\n");
-          out.write("                                            </form>\r\n");
-          out.write("                                        </div>\r\n");
-          out.write("                                    </div>\r\n");
-          out.write("\r\n");
+          out.write("                        <div class=\"swiper-slide card\">\r\n");
+          out.write("                            <div class=\"card-content\">\r\n");
+          out.write("                                <div class=\"image\">\r\n");
+          out.write("                                    <a href=\"detail?pid=");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.id}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("\"><img src=\"");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.image}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("\" alt=\"\" /></a>\r\n");
           out.write("                                </div>\r\n");
           out.write("\r\n");
-          out.write("                                <div class=\"product-body text-center\">\r\n");
-          out.write("                                    <h2 class=\"product-name\">\r\n");
-          out.write("                                        <a\r\n");
-          out.write("                                            href=\"#####################################################################################\"\r\n");
-          out.write("                                            title=\"Áo Thun Degrey Nhiễu Trắng - ATDN Trắng\"\r\n");
-          out.write("                                            >Áo Thun Degrey Nhiễu Trắng - ATDN Trắng</a\r\n");
-          out.write("                                        >\r\n");
-          out.write("                                    </h2>\r\n");
-          out.write("                                    <div class=\"product-price\">\r\n");
-          out.write("                                        <span class=\"amout\">\r\n");
-          out.write("                                            <span>250,000₫</span> <del class=\"sale-price\">320,000₫</del>\r\n");
-          out.write("                                        </span>\r\n");
-          out.write("                                    </div>\r\n");
-          out.write("                                    <a\r\n");
-          out.write("                                        class=\"ts-viewdetail hidden\"\r\n");
-          out.write("                                        href=\"#####################################################################################\"\r\n");
-          out.write("                                        ><span class=\"icon icon-arrows-slim-right\"></span\r\n");
-          out.write("                                        ></a>\r\n");
+          out.write("                                <div class=\"name-pro\">\r\n");
+          out.write("                                    <span class=\"name\">");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.name}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</span>\r\n");
+          out.write("                                    <span class=\"pro-title\">");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.title}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</span>\r\n");
+          out.write("                                </div>\r\n");
+          out.write("\r\n");
+          out.write("                                <div class=\"button\">\r\n");
+          out.write("                                    <button class=\"aboutMe\"><a href=\"detail?pid=");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.id}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write('"');
+          out.write('>');
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.price}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("đ</a>\r\n");
+          out.write("                                    </button>\r\n");
+          out.write("                                    <button class=\"hireMe\">\r\n");
+          out.write("                                        <a href=\"#\">Add to Cart <i class='bx bx-cart-alt'></i></a>\r\n");
+          out.write("                                    </button>\r\n");
           out.write("                                </div>\r\n");
           out.write("                            </div>\r\n");
-          out.write("                            <div class=\"product__tag\"></div>\r\n");
           out.write("                        </div>\r\n");
           out.write("                    </div>\r\n");
           out.write("\r\n");
@@ -277,7 +206,7 @@ public final class Home_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_th_c_forEach_0.doCatch(_jspx_exception);
     } finally {
       _jspx_th_c_forEach_0.doFinally();
-      _jspx_tagPool_c_forEach_end_begin.reuse(_jspx_th_c_forEach_0);
+      _jspx_tagPool_c_forEach_var_items.reuse(_jspx_th_c_forEach_0);
     }
     return false;
   }
