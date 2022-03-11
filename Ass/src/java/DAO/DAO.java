509b4892204122e5b231b245191635514196b167
toanpv224 <toanpv224@gmail.com>
2022-03-11 21:12:54 +0700

    d

--- Ass/src/java/DAO/DAO.java
+++ Ass/src/java/DAO/DAO.java
@@ -34,7 +34,8 @@ public class DAO extends Context.BaseDAO {
                         rs.getString(3),
                         rs.getDouble(4),
                         rs.getString(5),
-                        rs.getString(6)));
+                        rs.getString(6),
+                        rs.getInt(9)));
             }
         } catch (Exception e) {
         }
--- Ass/src/java/Model/Product.java
+++ Ass/src/java/Model/Product.java
@@ -16,8 +16,19 @@ public class Product {
     private double price;
     private String title;
     private String decription;
+    private int amount;
 
     public Product() {
+    }
+
+    public Product(int id, String name, String image, double price, String title, String decription, int amount) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.title = title;
+        this.decription = decription;
+        this.amount = amount;
     }
 
     public Product(int id, String name, String image, double price, String title, String decription) {
@@ -77,6 +88,14 @@ public class Product {
         this.decription = decription;
     }
 
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
     @Override
     public String toString() {
         return "Product{" + "id=" + id + ", name=" + name + ", image=" + image + ", price=" + price + ", title=" + title + ", decription=" + decription + '}';
